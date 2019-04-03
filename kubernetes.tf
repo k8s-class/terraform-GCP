eb7a901204c2ad302babb426e6f578150f90b80f
k8s-class <44496537+k8s-class@users.noreply.github.com>
2019-04-03 09:31:34 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -2,6 +2,11 @@ resource "google_container_cluster" "gcp_kubernetes" {
   name               = "${var.cluster_name}"
   zone               = "us-west1-a"
   initial_node_count = "${var.gcp_cluster_count}"
+  remove_default_node_pool = true
+
+  node_pool {
+    name = "default-pool"
+  }
 
   additional_zones = [
     "us-west1-b",
@@ -26,5 +31,25 @@ resource "google_container_cluster" "gcp_kubernetes" {
     }
 
     tags = ["dev", "work"]
+  }
+}
+resource "google_container_node_pool" "primary_pool" {
+  name       = "primary-pool"
+  cluster    = "${google_container_cluster.primary.name}"
+  zone       = "us-west1-a"
+  node_count = "2"
+
+  node_config {
+    machine_type = "n1-standard-1"
+  }
+
+  autoscaling {
+    min_node_count = 2
+    max_node_count = 5
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
   }
 }
